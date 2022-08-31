93b2e9e7f8a2ab4b87cadb8da3f002541fd11c2c
Tom <t_hankinson@outlook.com>
2022-08-31 15:23:04 +0100

    resolved react routes. resolved page layouts issue with css

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import {React, useEffect} from "react";
 import { Navigate } from "react-router-dom";
 
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import { tokenLogin } from "../utils";
 
@@ -9,21 +9,28 @@ import { Uploads } from "../content/upload";
 
 export const Home =({user, setUser})=>{
 
+    useEffect(() => {
+        document.title = "File Upload";
+      }, []);
+    
+
     return(
-        <MainContainerDiv>
-            {/* {(!user && !localStorage.key('myToken')) && <Navigate to="/"/>}
-            {(!user && localStorage.key('myToken')) && async function(setUser){ await tokenLogin(setUser) } } */}
+        <>
+            {(!user && !localStorage.key('myToken')) && <Navigate to="/"/>}
+            {(!user && localStorage.key('myToken')) && async function(setUser){ await tokenLogin(setUser) } }
+            {/* {!user ? <Login user={user} setUser={setUser}/> : <Uploads />} */}
             <Uploads />
-        </MainContainerDiv>
+            
+        </>
     )
 }
 
-const MainContainerDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    min-height: 100vh;
-`
+// const MainContainerDiv = styled.div`
+//     display: flex;
+//     flex-direction: row;
+//     flex-wrap: wrap;
+//     justify-content: center;
+//     align-items: center;
+//     width: 100vw;
+//     min-height: 100vh;
+// `
